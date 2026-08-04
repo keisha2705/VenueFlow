c0a32a0eaae94570833cc149761e3598209f02b8
keisha2705 <moyokeisha8@gmail.com>
2026-08-04 09:48:59 +0200

    React Structure

---
+++ Pages/AvailableVenus.jsx
@@ -0,0 +1,11 @@
+// Gallery(different parts of the venue)
+// Booking Page perVenues
+// Available venues to book/ Unavailable venues to book
+// SignIn,SignUp & Sign Out
+// Checkout process
+// States Dashboard(SuperAdmin → Admin)
+// LandingPage
+// HomePage(Different Venues)
+// About Page/Contact Us
+
+
