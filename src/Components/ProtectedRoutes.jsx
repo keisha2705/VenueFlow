76270b5802f76db01972a751fd66263e9d035937
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-05 21:10:53 +0200

    resolved merge conflict

--- src/Components/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes.jsx
@@ -3,7 +3,6 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children, role }) {
     // Simulate user authentication (replace with actual user context)
     const user = JSON.parse(localStorage.getItem("user"));
-
     if (!user) {
         return <Navigate to="/" />;
     } else if (user.role !== role) {
