8680b8ed4042e5d9a3e77f8d2793bfca3d849632
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-05 21:05:59 +0200

    resolved merge conflicts

---
+++ src/Pages/Admin.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div>
+      <h1>welcome to entrv</h1>
+      <p>Where YOU! can book, Where YOU! can add an Event, Where YOU! wanna be a user.</p>
+    </div>
+  );
+}
+
+
