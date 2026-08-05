a825c7da6e87f7fb712761c163c9eabbf6feec10
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-05 22:01:11 +0200

    solved conflict for auth page

--- src/Pages/AuthPage.jsx
+++ src/Pages/AuthPage.jsx
@@ -1,21 +1,13 @@
 import { useState } from "react";
 import SignIn from "../Components/SignIn";
-<<<<<<< HEAD
 import SignUp from "../Components/SignUp";
-=======
-import Signup from "../Components/SignUp";
->>>>>>> d59dc7ed8172377f8d8386de5608eaa99c7736cd
 
 function AuthPage() {
  const [showSignup, setShowSignup] = useState(true);
  return (
     <>
       {showSignup ? (
-<<<<<<< HEAD
          <SignUp showLogin={() => setShowSignup(false)}/>): (
-=======
-         <Signup showLogin={() => setShowSignup(false)}/>): (
->>>>>>> d59dc7ed8172377f8d8386de5608eaa99c7736cd
          <SignIn showSignup={() => setShowSignup(true)}/>)
       }
     </>
