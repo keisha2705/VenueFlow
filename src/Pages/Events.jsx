41ac74df6ab9d35502689030cb95fee95cd91d2a
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-11 20:49:52 +0200

    manager is able to create a venue

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -1,11 +1,11 @@
-// Gallery(different parts of the venue)
-// Booking Page perVenues
-// Available venues to book/ Unavailable venues to book
-// SignIn,SignUp & Sign Out
-// Checkout process
-// States Dashboard(SuperAdmin → Admin)
-// LandingPage
-// HomePage(Different Venues)
-// About Page/Contact Us
+import React from 'react'
 
+function Events() {
+  return (
+    <div>
+      <h1>EVENTS</h1>
+    </div>
+  )
+}
 
+export default Events
