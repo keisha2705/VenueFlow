99f1e90889f442ed66e2bbabfad9d942e6c661f0
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-08-12 11:29:22 +0200

    cleaned up the code

--- src/Pages/Manager.jsx
+++ src/Pages/Manager.jsx
@@ -1,27 +1,15 @@
 import { useNavigate } from "react-router-dom";
-
 function Manager() {
     const navigate = useNavigate();
-
     return (
         <div>
             <h1>Manager Dashboard</h1>
-
             <p>Welcome Manager!</p>
-
-            <button onClick={() => navigate("/events")}>
-                View Events
-            </button>
-
-            <button onClick={() => navigate("/bookings")}>
-                My Bookings
-            </button>
-
-            <button onClick={() => navigate("/manager/venues")}>
-                Manage Venues
-            </button>
+            <button onClick={() => navigate("/events")}> View Events </button>
+            <button onClick={() => navigate("/bookings")}> My Bookings</button>
+            <button onClick={() => navigate("/manager/venues")}> Manage Venues</button>
         </div>
     );
 }
 
-export default Manager;
+export default Manager
