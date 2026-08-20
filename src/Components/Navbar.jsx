cc50e986eb266064bc79dd66831f40310b183911
David <pokusd.ace@gmail.com>
2026-08-20 10:21:03 +0200

    protected navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,44 @@
+// import React from 'react';
+// import '../Styling/Navbar.css';
+
+// function Navbar() {
+//   return (
+//     <nav className="navbar glass">
+
+//       {/* Logo */}
+//       <div className="logo">
+//         eshek
+//       </div>
+
+    
+//       <div className="nav-links">
+//         <a href="/user">Home</a>
+//         <a href="/events">All Events</a>
+//         <a href="/coming-soon">Coming Soon</a>
+//         <a href="/application">Application Form</a>
+//         <a href="/">Dashboard</a>
+//         <a href="/about">About Us</a>
+//         <a href="booking">BookingPage</a>
+//       </div>
+
+//       {/* Profile */}
+//       <div className="profile-button">
+//        pfp
+//       </div>
+
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
+
+
 import React from 'react';
 import '../Styling/Navbar.css';
 
 function Navbar() {
+  const userRole = localStorage.getItem("userRole") || "user";
+
   return (
     <nav className="navbar glass">
 
@@ -10,20 +47,24 @@ function Navbar() {
         eshek
       </div>
 
-    
+      {/* Navigation Options Links */}
       <div className="nav-links">
         <a href="/user">Home</a>
         <a href="/events">All Events</a>
         <a href="/coming-soon">Coming Soon</a>
         <a href="/application">Application Form</a>
-        <a href="/">Dashboard</a>
+        
+        {(userRole === "superAdmin" || userRole === "manager") && (
+          <a href="/">Dashboard</a>
+        )}
+        
         <a href="/about">About Us</a>
         <a href="booking">BookingPage</a>
       </div>
 
       {/* Profile */}
       <div className="profile-button">
-       pfp
+        pfp
       </div>
 
     </nav>
